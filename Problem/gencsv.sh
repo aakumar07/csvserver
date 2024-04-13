ba4825990a5d366509ee76dd627f1a1f131025e3
aakumar07 <126548776+aakumar07@users.noreply.github.com>
2024-04-13 20:28:41 +0530

    Create gencsv.sh

---
+++ Problem/gencsv.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+# Defing thr index arrgements
+
+if [ "$#" -ne 8 ]; then
+    echo "given value in between the range <start_index> <end_index>"
+    exit 1
+fi
+
+# Difing the index values
+start_index=$2
+end_index=$8
+
+# Check if start index is not grater than end index
+if [ "$start_index" -ge "$end_index" ]; then
+    echo "Error: Start index must be less than end index"
+    exit 1
+fi
+
+# Generate CSV file with random numbers
+filename="inputFile.csv"
+echo "Index,Number" > "$filename"
+for ((i = start_index; i <= end_index; i++)); do
+    echo "$i, $((RANDOM % 1000))" >> "$filename"
+done
+
+echo "CSV file generated: $filename"
